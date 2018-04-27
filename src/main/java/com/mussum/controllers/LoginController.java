cb19b642e9e438c6e25c0afa83aa30b8c87a39c1
Yuri Bento <yurinbento@hotmail.com>
2018-04-27 01:40:07 -0300

    retornando token

--- src/main/java/com/mussum/controllers/LoginController.java
+++ src/main/java/com/mussum/controllers/LoginController.java
@@ -8,6 +8,8 @@ package com.mussum.controllers;
 import com.mussum.models.Usuario;
 import com.mussum.repository.ProfessorRepository;
 import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,18 +29,23 @@ public class LoginController {
 	System.out.println("AUTENTICANDO USUARIO:");
 	System.out.println(usuario.getUsername());
 	System.out.println(usuario.getPassword());
-	
-	String token = "huhuh kokokokokokkookkooko";
-	
+
+	String token = "Bearer " + Jwts.builder()
+		.setSubject(usuario.getUsername())
+		.signWith(SignatureAlgorithm.HS512, "mussum")
+		.compact();
+
 	return new TokenResponse(token);
-	
+
 	//procurar por um professor no banco que bata com os dados recebidos (username/pass)
 	//se o professor existe, gerar um token e devolver pro client
 	//se nao, bom.. dai é obvio
     }
-    
+
     private class TokenResponse {
+
 	public String token;
+
 	public TokenResponse(String token) {
 	    this.token = token;
 	}
