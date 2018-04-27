9da49cc87c06dbcf864862a03313dab096c0b937
Yuri Bento <yurinbento@hotmail.com>
2018-04-27 01:30:19 -0300

    funcionando rota login, respondendo token fake

--- src/main/java/com/mussum/controllers/LoginController.java
+++ src/main/java/com/mussum/controllers/LoginController.java
@@ -7,6 +7,7 @@ package com.mussum.controllers;
 
 import com.mussum.models.Usuario;
 import com.mussum.repository.ProfessorRepository;
+import io.jsonwebtoken.JwtParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,19 +22,26 @@ public class LoginController {
     private ProfessorRepository rep;
 
     @PostMapping()
-    public String autenticar(@RequestBody Usuario usuario) {
+    public TokenResponse autenticar(@RequestBody Usuario usuario) {
 
 	System.out.println("AUTENTICANDO USUARIO:");
 	System.out.println(usuario.getUsername());
 	System.out.println(usuario.getPassword());
 	
+	String token = "huhuh kokokokokokkookkooko";
 	
-	
-	return "logado pra caralho";
+	return new TokenResponse(token);
 	
 	//procurar por um professor no banco que bata com os dados recebidos (username/pass)
 	//se o professor existe, gerar um token e devolver pro client
 	//se nao, bom.. dai é obvio
+    }
+    
+    private class TokenResponse {
+	public String token;
+	public TokenResponse(String token) {
+	    this.token = token;
+	}
     }
 
 }
