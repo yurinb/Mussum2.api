933cfce09792719a9dbad1c7d42720b47698f889
Yuri Bento <yurinbento@hotmail.com>
2018-04-27 00:00:50 -0300

    desabilitando security

---
+++ src/main/java/com/mussum/models/Aviso.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mussum.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aviso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+}
