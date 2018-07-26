e0e140bfd30e2143c41c840f2e7b5b3d154eecfa
Yuri Bento <yurinbento@hotmail.com>
2018-07-25 21:49:37 -0300

    Fixes #24 hide password

--- src/main/java/com/mussum/controllers/security/LoginController.java
+++ src/main/java/com/mussum/controllers/security/LoginController.java
@@ -1,12 +1,12 @@
 package com.mussum.controllers.security;
 
 import com.mussum.models.db.Professor;
-import com.mussum.models.db.Usuario;
 import com.mussum.repository.ProfessorRepository;
 import com.mussum.util.S;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,64 +23,64 @@ public class LoginController {
     private ProfessorRepository rep;
 
     @PostMapping()
-    public ResponseEntity<?> autenticar(@RequestBody Usuario usuario) {
+    public ResponseEntity<?> autenticar(@RequestBody Map<String, String> user) {
 
-        S.out("_____________AUTH_____________", this);
-        S.out("username: " + usuario.getUsername(), this);
-        S.out("password: " + usuario.getPassword(), this);
+	S.out("_____________AUTH_____________", this);
+	S.out("username: " + user.get("username"), this);
+	S.out("password: " + user.get("password"), this);
 
-        if (usuario.getUsername() == null || usuario.getPassword() == null) {
+	if (user.get("username") == null || user.get("password") == null) {
 
-            S.out("ERROR: user or password are NULL", this);
-            S.out("_______________*_______________", this);
-            return new ResponseEntity<>("usuario e senha obrigatório!", HttpStatus.UNAUTHORIZED);
+	    S.out("ERROR: user or password are NULL", this);
+	    S.out("_______________*_______________", this);
+	    return new ResponseEntity<>("usuario e senha obrigatório!", HttpStatus.UNAUTHORIZED);
 
-        }
+	}
 
-        S.out("Authenticating...", this);
+	S.out("Authenticating...", this);
 
-        Professor usuarioEncontrado = rep.findByUsername(usuario.getUsername());
-        if (usuarioEncontrado == null) {
+	Professor usuarioEncontrado = rep.findByUsername(user.get("username"));
+	if (usuarioEncontrado == null) {
 
-            S.out("ERROR: inexistent or invalid user", this);
-            S.out("_______________*_______________", this);
-            return new ResponseEntity<>("usuario inválido ou inexistente.", HttpStatus.UNAUTHORIZED);
-        }
+	    S.out("ERROR: inexistent or invalid user", this);
+	    S.out("_______________*_______________", this);
+	    return new ResponseEntity<>("usuario inválido ou inexistente.", HttpStatus.UNAUTHORIZED);
+	}
 
-        if (!usuarioEncontrado.getPassword().equals(usuario.getPassword())) {
+	if (!usuarioEncontrado.getPassword().equals(user.get("password"))) {
 
-            S.out("ERROR: invalid password", this);
-            S.out("_______________*_______________", this);
-            return new ResponseEntity<>("senha inválida", HttpStatus.UNAUTHORIZED);
-        }
+	    S.out("ERROR: invalid password", this);
+	    S.out("_______________*_______________", this);
+	    return new ResponseEntity<>("senha inválida", HttpStatus.UNAUTHORIZED);
+	}
 
-        S.out("Authenticated.", this);
-        S.out("_______________*_______________", this);
+	S.out("Authenticated.", this);
+	S.out("_______________*_______________", this);
 
-        String token = Jwts.builder()
-                .setSubject(usuario.getUsername())
-                .signWith(SignatureAlgorithm.HS512, "mussum")
-                .setExpiration(new Date(System.currentTimeMillis() + (9999 * 60 * 1000)))
-                .compact();
+	String token = Jwts.builder()
+		.setSubject(user.get("username"))
+		.signWith(SignatureAlgorithm.HS512, "mussum")
+		.setExpiration(new Date(System.currentTimeMillis() + (9999 * 60 * 1000)))
+		.compact();
 
-        String professorName = rep.findByUsername(usuarioEncontrado.getUsername()).getNome();
+	String professorName = rep.findByUsername(usuarioEncontrado.getUsername()).getNome();
 
-        return new ResponseEntity<>(new TokenResponse(token, usuarioEncontrado.getRole(), professorName), HttpStatus.OK);
+	return new ResponseEntity<>(new TokenResponse(token, usuarioEncontrado.getRole(), professorName), HttpStatus.OK);
     }
 
     private class TokenResponse {
 
-        public String token;
+	public String token;
 
-        public String role;
+	public String role;
 
-        public String nome;
+	public String nome;
 
-        public TokenResponse(String token, String role, String nome) {
-            this.token = token;
-            this.role = role;
-            this.nome = nome;
-        }
+	public TokenResponse(String token, String role, String nome) {
+	    this.token = token;
+	    this.role = role;
+	    this.nome = nome;
+	}
     }
 
 }
--- src/main/java/com/mussum/models/db/Usuario.java
+++ src/main/java/com/mussum/models/db/Usuario.java
@@ -1,5 +1,6 @@
 package com.mussum.models.db;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mussum.models.MussumEntity;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
@@ -18,35 +19,35 @@ public class Usuario extends MussumEntity {
     public void setDefaultRole() {
 	this.role = "professor";
     }
-    
+
     public String getUsername() {
-        return username;
+	return username;
     }
 
     public void setUsername(String username) {
-        this.username = username;
+	this.username = username;
     }
 
+    @JsonIgnore
     public String getPassword() {
-        return password;
+	return password;
     }
 
     public void setPassword(String password) {
-        this.password = password;
+	this.password = password;
     }
 
     public String getRole() {
-        return role;
+	return role;
     }
 
     public void setRole(String roles) {
-        this.role = roles;
+	this.role = roles;
     }
 
 //    public void addRole(String role) {
 //        this.role += "," + role;
 //    }
-
 //    public void removeRole(String role) {
 //        String[] currentRoles = this.role.split(",");
 //        String newRoles = "";
@@ -63,5 +64,4 @@ public class Usuario extends MussumEntity {
 //        }
 //        this.role = newRoles;
 //    }
-
 }
